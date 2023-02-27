6e3b888dd33d00da3a392ec19bf0bed84a90356c
Arrq54 <arek.wojdyla@interia.pl>
2023-02-27 23:58:20 +0100

    charts

---
+++ countries-data/src/components/Charts/Chart.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { CountryPopulationName } from '../../interfaces/CountryPopulationName'
+import {PieChart, Pie,Cell,Tooltip, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer } from 'recharts';
+import "../../styles/Chart.css"
+export default function Chart(props: {data: CountryPopulationName[], type:string, width:number, height:number, radius:number}) {
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    let renderLabel = function(entry:CountryPopulationName) {
+        return entry.name;
+    }
+    let chart;
+    switch(props.type){
+        case "piechart20":
+            chart = <PieChart width={props.width} height={props.height}>
+            <Pie
+              dataKey="population"
+              isAnimationActive={false}
+              data={props.data}
+              cx="50%"
+              cy="50%"
+              outerRadius={props.radius}
+              fill="#8884d8"
+              nameKey="name"
+              // label={renderLabel}
+            >
+              {props.data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+            break;
+        case "barchart20":
+            chart = <BarChart
+            width={props.width}
+            height={props.height}
+            data={props.data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="population" fill="#8884d8" />
+          </BarChart>
+            break;
+        default: break;
+    }
+
+    return (
+    <div className='m-20'>
+         {chart}
+    </div>
+  )
+}
--- countries-data/src/components/Charts/Charts.tsx
+++ countries-data/src/components/Charts/Charts.tsx
@@ -1,28 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Menu from '../MainPage/Menu'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import "../../styles/Charts.css"
+
 import { CountryPopulationName } from '../../interfaces/CountryPopulationName';
+import Chart from './Chart';
 
 export default function Charts() {
   const [data, setData] = useState<CountryPopulationName[]>([]);
+  const [largest20, setLargest20] = useState<CountryPopulationName[]>([]);
     const navigate = useNavigate();
 
     useEffect(()=>{
       fetch("/countryNamePopulation").then(data=>data.json()).then((data)=>{
         // data.map((i:CountryPopulationName)=>{i.population = parseInt(parseInt(i.population.replace(",",""))/1000000)})
+      console.log(data);
+      data.map((i:CountryPopulationName)=>{
+        let x:string =String(i.population).replace(",","")
+        i.population = parseInt(x);  
+      })
+      setData(data.sort((a:CountryPopulationName,b:CountryPopulationName)=>{return b.population - a.population}))
+     
+      let l10:CountryPopulationName[] = data.slice(0,15);
+      let otherpop:number = 0;
+      data.slice(15,data.length).map((i:CountryPopulationName)=>{
+        otherpop += i.population;
+      });
+      l10.push({name: "Other countries", population: otherpop})
+
+      setLargest20(l10)
+      
+
       setData(data)})
       
       
     },[])
+    
+  
   return (
-    <div> 
+    <div >
+       
         <Menu chosen={"Charts"} onItemClick={navigate}/>
-          <BarChart width={900} height={500} data={data}>
-            <Bar type="monotone" dataKey="population" fill="#333333" layout='vertical' />
-            <XAxis dataKey="name" />
-            <YAxis />
-          </BarChart>
+        <div className='charts'>
+          <div className='chart'>
+            <h3>Population pie chart</h3>
+            <Chart data={largest20} type={"piechart20"} width={500} height={240} radius={120} />
+          </div> 
+
+          <div className='chart'>
+            <h3>Population bar chart</h3>
+            <Chart data={largest20} type={"barchart20"} width={500} height={240} radius={120} />
+          
+          </div> 
+        </div>
+        
+       
         </div>
   )
 }
